6ecada9099f74d1c5c3776779ce66710a3a9a535
Viktor Martinović <53435298+martino-vic@users.noreply.github.com>
2021-12-30 00:58:28 +0000

    forgot brackets, classic

--- ipatok/__init__.py
+++ ipatok/__init__.py
@@ -1,2 +1,2 @@
-from .tokens import tokenise, tokenize, clusterise, clusterize,
-replace_digits_with_chao
+from .tokens import (tokenise, tokenize, clusterise, clusterize,
+replace_digits_with_chao)
